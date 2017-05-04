a5c4c2a76edb74ce8a95bc268b79cd4c9bb44367
Salvatore Stella <sstella@ncsu.edu>
2017-05-04 12:09:36 +0200

    more cleanups

--- minor_evaluation.py
+++ minor_evaluation.py
@@ -4,8 +4,7 @@ class MinorEvaluation(SageObject):
     
     def __init__(self, X, element=None):
         self._C = CartanType(X)
-        self._P = self._C.root_system().weight_space()
-        self._W = self._P.weyl_group()
+        self._P = self._C.root_system().weight_space(extended = self._C.is_affine())
         if element == None:
             self.set_generic_element()
         elif set(element) == set(self._C.index_set()):
@@ -17,7 +16,7 @@ class MinorEvaluation(SageObject):
         self._g = tuple(element)
 
     def set_generic_element(self):
-        w0 = self._W.long_element().reduced_word()
+        w0 = self._P.weyl_group().long_element().reduced_word()
         element = [ ('f', i, var('t%s'%k)) for (k,i) in enumerate(w0) ]
         element += [ ('h', k, var('h%s'%k)) for k in set(self._C.index_set()) ]
         element += reversed([ ('e', i, var('s%s'%k)) for (k,i) in enumerate(w0) ])
@@ -42,24 +41,16 @@ class MinorEvaluation(SageObject):
         # sanitize input
         la = self._P.from_vector(vector(la))
         mu = self._P.from_vector(vector(mu))
-
-        if not crystal_generator:
-            crystal_generator = la.to_dominant_chamber()
-        # NOTE TO SELF: here crystals.LSPaths builds the wrong crystal if crystal_generator is an element of self._P. Is this a bug in sage?
-        # WORKAROUND: use vectors instead
-        # EXAMPLE:
-        # sage: C = CartanType(['A',2])
-        # sage: P = C.root_system().weight_space()
-        # sage: W = P.weyl_group()
-        # sage: la = P.from_vector(vector((1,0)))
-        # sage: la
-        # Lambda[1]
-        # sage: crystals.LSPaths(C,la)
-        # The crystal of LS paths of type ['A', 1] and weight Lambda[2]
-        crystal = crystals.LSPaths(self._C, vector(crystal_generator))
-
-        # The following line assumes that la is a 1-dimensional weight space in the representation otherwise picks the first path of weight la
-        initial_path = ifilter(lambda v: v.weight() == la, crystal).next()
+        
+        if crystal_generator:
+            # this assumes that the representation is highest weight (because of how sage.combinat.crystals.crystals.CrystalBacktracker is implemented)
+            crystal_generator = self._P.from_vector(vector(crystal_generator)).to_dominant_chamber()
+            crystal = crystals.LSPaths(crystal_generator)
+            initial_path = ifilter(lambda v: v.weight() == la, crystal).next()
+        else: # by default compute minors in the smallest irreducible representations having the required weight as extremal 
+            crystal = crystals.LSPaths(la)
+            initial_path = crystal((la,))
+        
         paths = [(initial_path,1)]
         for (l,i,x) in reversed(self._g):
             new_paths = []
@@ -74,4 +65,3 @@ class MinorEvaluation(SageObject):
                         wt = getattr(wt, l)(i)
             paths = new_paths
         return sum([ mon for (wt,mon) in paths if wt.weight() == mu ])
-                    
